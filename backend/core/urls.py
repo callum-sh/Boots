585cac0f6994e4f42a85807891cf591813b87827
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2025-01-12 12:08:59 -0500

    frontend init

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -31,7 +31,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('competitions.urls')),
