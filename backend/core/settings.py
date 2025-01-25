1476b4df4db651c4246fa1079d2c5a7ec0968297
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2025-01-25 17:38:20 -0500

    ios build content

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,11 +44,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     # apps
     "competitions",
     "invitations",
-    # 'authentication',
-    # REST API
+    
+    # REST framework 
     "rest_framework",
     "rest_framework.authtoken",
     "drf_yasg",
@@ -168,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
